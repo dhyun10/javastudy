2918b98798fdd68f723b65120586fc580be2ef69
김 다현 <d-hyun10@hanmail.net>
2021-04-18 19:36:01 +0900

    exception

---
+++ ch08/ExceptionEx1.java
@@ -0,0 +1,25 @@
+
+public class ExceptionEx1 {
+
+	public static void main(String[] args) {
+		try {
+			try { } catch (Exception e) {
+				
+			}
+		} catch (Exception e) {
+			try {
+				
+			} catch (Exception e2) {
+			
+			}
+		}
+		
+		try {
+			
+		} catch (Exception e) {
+			
+			
+		}
+	}
+
+}
---
+++ ch08/ExceptionEx10.java
@@ -0,0 +1,8 @@
+
+public class ExceptionEx10 {
+
+	public static void main(String[] args) {
+		throw new Exception();
+	}
+
+}
